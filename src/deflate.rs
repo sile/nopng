52a1e3e9b8c7c6c36b2b869bb6625919eb63332c
Takeru Ohta <phjgt308@gmail.com>
2025-05-22 18:42:59 +0900

    Refactor DeflateNoCompressionEncoder to use stateless design pattern

--- src/deflate.rs
+++ src/deflate.rs
@@ -1,22 +1,11 @@
 use std::io::Write;
 
 #[derive(Debug)]
-pub struct DeflateNoCompressionEncoder<W> {
-    //
-    inner: W,
-    data: Vec<u8>,
-}
+pub struct DeflateNoCompressionEncoder;
 
-impl<W: Write> DeflateNoCompressionEncoder<W> {
-    pub fn new(writer: W, data: Vec<u8>) -> Self {
-        Self {
-            inner: writer,
-            data,
-        }
-    }
-
-    pub fn encode(&mut self) -> std::io::Result<()> {
-        let mut remaining = self.data.len();
+impl DeflateNoCompressionEncoder {
+    pub fn encode<W: Write>(&mut self, writer: &mut W, data: &[u8]) -> std::io::Result<()> {
+        let mut remaining = data.len();
         let mut offset = 0;
 
         while remaining > 0 {
@@ -28,23 +17,23 @@ impl<W: Write> DeflateNoCompressionEncoder<W> {
 
             // Write block header: 1 bit for final block, 2 bits for block type (00 = no compression)
             let header_byte = if is_final { 0b0000_0001 } else { 0b0000_0000 };
-            self.inner.write_all(&[header_byte])?;
+            writer.write_all(&[header_byte])?;
 
             // Write LEN and NLEN (LEN's one's complement) - 16-bit values in little-endian
             let len = (size as u16).to_le_bytes();
             let nlen = (!size as u16).to_le_bytes();
 
-            self.inner.write_all(&len)?;
-            self.inner.write_all(&nlen)?;
+            writer.write_all(&len)?;
+            writer.write_all(&nlen)?;
 
             // Write the actual data for this block
-            self.inner.write_all(&self.data[offset..offset + size])?;
+            writer.write_all(&data[offset..offset + size])?;
 
             // Update counters
             offset += size;
             remaining -= size;
         }
 
-        self.inner.flush()
+        writer.flush()
     }
 }
